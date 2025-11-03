eb08201d1258f9b2e11acce5c2ae34bbfe14d7f1
yevheniia <moiseevaevgenya16@gmail.com>
2025-11-03 17:53:20 +0200

    add tests

--- app/constants.rb
+++ app/constants.rb
@@ -5,7 +5,7 @@ module Constants
   SETTINGS_PLACE = File.join(ROOT_DIR, 'config', 'elastic', 'elasticsearch.yaml').freeze
 
   module Elasticsearch
-    INDEX_NAME = ENV.fetch('ELASTIC_INDEX_NAME', 'cache_events_v1').freeze
-    INDEX_ALIAS = ENV.fetch('ELASTIC_INDEX_ALIAS', 'cache_events').freeze
+    INDEX_NAME = ENV.fetch('ELASTIC_INDEX_NAME').freeze
+    INDEX_ALIAS = ENV.fetch('ELASTIC_INDEX_ALIAS').freeze
   end
 end
