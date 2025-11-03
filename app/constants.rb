12a4161f36a3124a6ad3cd152b4baadea6dd0445
yevheniia <moiseevaevgenya16@gmail.com>
2025-11-03 17:27:14 +0200

    add tests

--- app/constants.rb
+++ app/constants.rb
@@ -2,7 +2,7 @@ module Constants
   ROOT_DIR = File.expand_path('../../', __FILE__).freeze
 
   MAPPING_PLACE = File.join(ROOT_DIR, 'config', 'elastic', 'mapping.yaml').freeze
-  SETTINGS_PLACE = File.join(ROOT_DIR, 'config', 'elastic', 'elasticsearch.yml').freeze
+  SETTINGS_PLACE = File.join(ROOT_DIR, 'config', 'elastic', 'elasticsearch.yaml').freeze
 
   module Elasticsearch
     INDEX_NAME = ENV.fetch('ELASTIC_INDEX_NAME', 'cache_events_v1').freeze
--- config.ru
+++ config.ru
@@ -1,7 +1,3 @@
-# config.ru
-
-# Используем абсолютный путь к текущей директории, который Docker гарантирует
-# ENV['RACK_ROOT'] обычно устанавливается в корневую директорию Docker WORKDIR (/app)
 require File.expand_path('../app.rb', __FILE__)
 
 run Router
